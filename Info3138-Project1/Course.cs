701d39d6ec1aa437c1b7cdfeba1a245c6a97a510
Daniel Diaz Forero <d_diazforero@fanshaweonline.ca>
2025-06-01 16:33:45 -0400

    Initial setup with models, NuGet packages, and schema/data files

---
+++ Info3138-Project1/Course.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Info3138_Project1
+{
+    internal class Course
+    {
+        //Creating the properties of the Course class
+        public string CourseCode { get; set; }  //Defines the course,class code (Ex. INFO3138, INFO4123)
+        public List<Evaluation> Evaluations { get; set; } = new();  //A list that will hold all the properties of the activity, evaluation for the Course
+    }
+}
---
+++ Info3138-Project1/Evaluation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Info3138_Project1
+{
+    internal class Evaluation
+    {
+        //Creating the properties of the Evaluations for a course, class. (Ex. Assignments, Quizzes, Projects, etc)
+        public string Description { get; set; } //A little description of what the Evaluation is about and which kind of evaluation is
+        public double Weight { get; set; }  //The % of the course mark attributed to the evaluation
+        public int OutOf { get; set; }  //The number of marks that represents a perfect score on the evaluation
+        public double? EarnedMarks { get; set; }  //The student’s score on the evaluation
+    }
+}
