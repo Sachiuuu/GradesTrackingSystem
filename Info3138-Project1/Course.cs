ae140325bb1a3e8d276a529d72b4a84d8b8621bf
Daniel Diaz Forero <d_diazforero@fanshaweonline.ca>
2025-06-01 18:23:42 -0400

    deleteted schema, and fixed nullable properties

--- Info3138-Project1/Course.cs
+++ Info3138-Project1/Course.cs
@@ -9,7 +9,7 @@ namespace Info3138_Project1
     internal class Course
     {
         //Creating the properties of the Course class
-        public string CourseCode { get; set; }  //Defines the course,class code (Ex. INFO3138, INFO4123)
+        public string? CourseCode { get; set; }  //Defines the course,class code (Ex. INFO3138, INFO4123)
         public List<Evaluation> Evaluations { get; set; } = new();  //A list that will hold all the properties of the activity, evaluation for the Course
     }
 }
--- Info3138-Project1/Evaluation.cs
+++ Info3138-Project1/Evaluation.cs
@@ -9,7 +9,7 @@ namespace Info3138_Project1
     internal class Evaluation
     {
         //Creating the properties of the Evaluations for a course, class. (Ex. Assignments, Quizzes, Projects, etc)
-        public string Description { get; set; } //A little description of what the Evaluation is about and which kind of evaluation is
+        public string? Description { get; set; } //A little description of what the Evaluation is about and which kind of evaluation is
         public double Weight { get; set; }  //The % of the course mark attributed to the evaluation
         public int OutOf { get; set; }  //The number of marks that represents a perfect score on the evaluation
         public double? EarnedMarks { get; set; }  //The student’s score on the evaluation
